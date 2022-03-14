a30c8ca2150ae5d229a83f316616a3d00875be34
Matthew Goodrich <development@matthewgoodrich.com>
2022-03-14 15:07:09 -0700

    Update hardhat config to .secret

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,19 @@
-// https://rinkeby.infura.io/v3/8d89be08e82f47a89a2a847cfc7b69e6
-
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "8d89be08e82f47a89a2a847cfc7b69e6";
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
     solidity: "0.8.0",
     networks: {
         rinkeby: {
-            url: "https://rinkeby.infura.io/v3/8d89be08e82f47a89a2a847cfc7b69e6",
-            accounts: [
-                "ca602d5152e08f6a1bd05e85678be08915b3650531f39a910f61396792451a27",
-            ],
+            url: `https://rinkeby.infura.io/v3/${projectId}`,
+            accounts: [privateKey],
+        },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${projectId}`,
+            accounts: [privateKey],
         },
     },
 };
